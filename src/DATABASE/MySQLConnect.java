e3874059c3d296f2ae84d70f2cd273c2c52a25d4
Tran Hoai Nam <namth3122410252@gmail.com>
2024-09-30 17:22:04 +0700

    chưa create đc excel

--- src/DATABASE/MySQLConnect.java
+++ src/DATABASE/MySQLConnect.java
@@ -14,7 +14,7 @@ import java.util.logging.Logger;
  * @author PHUONG ANH
  */
 public class MySQLConnect {
-    private String url="jdbc:mysql://localhost:3306/student_management?zeroDateTimeBehavior=CONVERT_TO_NULL";
+    private String url="jdbc:mysql://localhost:3307/student_management?zeroDateTimeBehavior=CONVERT_TO_NULL";
     private String user="root";
     private String password="";
     private Connection conn=null;
